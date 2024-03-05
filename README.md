35e9afec1897f6b0461bf4dee37678bb41df2be7
L.lar <l.lar1109@gmail.com>
2024-03-05 04:07:02 -0500

    completed webpack config

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Text-Editor
+
+## Description
+
+Create a browser-based text editor as a single-page application, fulfilling PWA standards. Implement various data persistence methods for redundancy and ensure offline functionality.
+
+## Table of Contents
+
+- [Technologies](#technologies)
+- [Installation](#installation)
+- [Mock Up](#mock-up)
+
+
+## Technologies
+
+```md
+
+```
+
+## Installation
+
+
+
+## Mock Up
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,16 +14,67 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      // workbox plugins for service worker
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js'
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        name: 'My Text-Editor Web App',
+        short_name: 'MyTE',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', 
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+  
     ],
 
     module: {
       rules: [
-        
+        // add css loaders and babel to webpack
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            }
+          ]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
