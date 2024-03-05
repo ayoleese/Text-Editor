dcb5f24983541d71d402cdc873fa76fd9791dc30
L.lar <l.lar1109@gmail.com>
2024-03-05 04:36:02 -0500

    completed database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,51 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+// creates database name jate, version 1
   openDB('jate', 1, {
+    // sets up database schema if it does not already defined
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // creates collection inside jate datebase name jate
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Export a function in which we will use POST not put
+export const postDb = async (content) => {
+  console.log('Post to the database');
+// creates conection to the jateDB datebase 
+  const jateDb = await openDB('jate', 1);
+// create transaction to jate collection with a read/write data priviledege
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // open up desire object store
+  const store = tx.objectStore('jate');
+  // use add(). method on the store and pass the content
+  const request = store.add({ jate: content });
+// confirmation of the request
+  const result = await request;
+  console.log( 'Data saved to the database', result );
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getAllDb = async () => {
+  console.log('GET all from the database');
+
+  const jateDb = await openDB('jate', 1);
+// create transaction to jate collection with a read only data priviledege
+  const tx = jateDb.transaction('jate', 'readOnly');
+  const store = tx.objectStore('jate');
+  // use .getAll() method to retrieve all data in the database
+  const request = store.getALL();
+// confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
